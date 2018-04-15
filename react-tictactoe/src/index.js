4c79dfab88c50739f14de32d16eaba6f930e4d3d
Michael Heinisch <venomx411@gmail.com>
2018-04-15 10:50:21 -0500

    completed implementation of time travel

--- react-tictactoe/src/index.js
+++ react-tictactoe/src/index.js
@@ -59,11 +59,12 @@ class Game extends React.Component {
       history: [
         { squares: Array(9).fill(null) }
       ],
+      stepNumber: 0,
       xIsNext: true
     };
   }
   handleClick(i) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
     if(determineWinner(this.state.squares) || squares[i]) {
@@ -74,18 +75,25 @@ class Game extends React.Component {
       history: history.concat([{
         squares: squares
       }]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext
+    });
+  }
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0
     });
   }
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = determineWinner(current.squares);
 
     const moves = history.map((step, move) => {
       const desc = move ? 'Go to move #' + move : 'Go to game start';
       return (
-        <li>
+        <li key={move}>
           <button onClick={() => this.jumpTo(move)}>{desc}</button>
         </li>
       );
